5ef6472138aed32b8bf7448eb2b2541f5d3aafa1
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-05-11 20:45:28 +0200

    move css styles to styled components

--- src/square.jsx
+++ src/square.jsx
@@ -1,11 +1,35 @@
 import React from "react"
 import Proptypes from "prop-types"
+import styled from "styled-components"
+
+const Button = styled.button`
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 22px;
+  height: 22px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 22px;
+
+  &:focus {
+    outline: none;
+  }
+
+  .kbd-navigation &:focus {
+    background: #ddd;
+  }
+`
 
 const Square = ({ value, onClick }) => {
   return (
-    <button className="square" onClick={onClick}>
+    <Button className="square" onClick={onClick}>
       {value}
-    </button>
+    </Button>
   )
 }
 
