d32537bd999974d9148111776d172885773de0da
ibqn <evgen.ibqn@gmail.com>
2023-08-05 20:15:06 +0200

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
       fontFamily: {
         century: ['"Century Gothic"', "Futura", "sans-serif"],
       },
+      colors: {
+        grau: "#999",
+      },
     },
   },
   plugins: [],
