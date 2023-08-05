685a0f544c4b9056d83f3157bca37c30057d5119
ibqn <evgen.ibqn@gmail.com>
2023-08-05 20:01:35 +0200

    revert prettier setting

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   endOfLine: 'lf',
   semi: false,
   printWidth: 120,
-  singleQuote: true,
+  singleQuote: false,
   tabWidth: 2,
   trailingComma: 'es5',
   plugins: ['prettier-plugin-tailwindcss'],
